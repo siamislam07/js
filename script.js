d2edb2c058f8a37a7e045e508e14b4392f267ff4
siamislam07 <siamislam1970@gmail.com>
2022-12-13 11:02:15 +0600

    first commmit

---
+++ script.js
@@ -0,0 +1,49 @@
+// var x = 5;
+// var y = "siam";
+
+// console.log(typeof x);
+// console.log(typeof y);
+
+
+// var isSiam = true;
+// var isRamjan = false;
+
+// console.log(typeof isSiam);
+
+
+// var md = null;
+
+// console.log(typeof md);
+
+// var x = 26.65;
+// var y = 122;
+
+// console.log(typeof x);
+// console.log (typeof y);
+ 
+var age = [29, 40, 34];
+
+console.log(typeof age);
+
+var age = ['siam', 'islam',]
+
+console.log(typeof age)
+
+var friend = {firstName:"din", lastName:"islam", age:17, bodyColor:"white"};
+console.log(typeof friend);
+
+
+
+// var x = 10;
+// var y = "5";
+// y = parseInt(y);
+// x = parseFloat(x)
+
+// console.log(typeof x);
+// console.log(typeof y);
+
+// var oldBalance = 200;
+// var newBalance = 100;
+// var totalBalance = oldBalance + newBalance
+//  console.log(totalBalance)
+
